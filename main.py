08e00a5bc5dff182f0b3e7aef5b2f7b505ecaa02
Aleksejs <alex@test.com>
2024-02-27 17:13:43 +0100

    minor

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import matplotlib.pyplot as plt
 
 from readers import classical_reader
 from problems import ClassicalProblem
-from solvers import BruteSolverV1, BruteSolverV2, BruteSolverV3
+from solvers import BruteSolverV1, BruteSolverV2, BruteSolverV3, BruteSolverV4
 
-grid = classical_reader('examples/grid_easy.dat')
+grid = classical_reader('examples/grid_hard.dat')
 problem = ClassicalProblem()
 
-times = []
-nTest = 50
-for iTest in range(nTest):
-    timeStart = time()
-    bs = BruteSolverV3(grid, problem)
-    bs.solve()
+bs = BruteSolverV1(grid, problem)
+bs.solve()
 
-    times += [time() - timeStart]
-
-plt.figure()
-plt.plot(np.sort(times), np.linspace(0, 1, nTest))
-plt.show()
+# times = []
+# nTest = 50
+# for iTest in range(nTest):
+#     timeStart = time()
+#     bs = BruteSolverV3(grid, problem)
+#     bs.solve()
+# 
+#     times += [time() - timeStart]
+# 
+# plt.figure()
+# plt.plot(np.sort(times), np.linspace(0, 1, nTest))
+# plt.show()
